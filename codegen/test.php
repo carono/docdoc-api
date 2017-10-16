30e33a06a6c3c23358e825f75b12520a788b8bb7
carono <info@carono.ru>
2017-10-16 19:45:07 +0500

    refs

--- codegen/test.php
+++ codegen/test.php
@@ -3,7 +3,5 @@ require_once dirname(__DIR__) . '/vendor/autoload.php';
 require_once dirname(__DIR__) . '/src/Client.php';
 
 $client = new \carono\docdoc\Client();
-$client->login = 'partner.3051';
-$client->password = 'CiWCZCBv';
 $x = $client->nearestStationGeo(40, 40);
 print_r($x);
